1f894d433b71479d70480248bcc933cebe83f1cc
Thomas Shafee <TS404@users.noreply.github.com>
2017-12-18 22:23:15 +1100

    OVERHAUL with RGLwidget

--- ui.R
+++ ui.R
@@ -6,6 +6,7 @@
 #
 
 library("shiny")
+library("rglwidget")
 shinyUI(fluidPage(
   # Header ---------------------------------------------
   titlePanel("Defensins Sequence Space"),
@@ -21,10 +22,11 @@ shinyUI(fluidPage(
           It can predict whether a given query sequence is a cis-defensin or trans-defensin, map
           its sequence space location, and determine which cluster it falls into.")
         )
-    ),
-
+        ),
+  
+  
   tabsetPanel(
-  # TAB 1 -----------------------------------
+    # TAB 1 -----------------------------------
     tabPanel("View",fluidPage(
       sidebarPanel(
         h3("Sequence Space"),
@@ -32,14 +34,14 @@ shinyUI(fluidPage(
         selectInput("view_type",
                     label   = "Defensin superfamily",
                     choices  = c("cis-Defensin","trans-Defensin")),
-        p("Clicking \"show\" will open the interactive diagram in a new window."),
+        p("Clicking \"show\" will open the interactive diagram"),
         actionButton("button.view", "Show"),
         tags$hr(style="border-color: grey;"),
-        h4("Identify sequences"),
-        p("To identify points in the sequence space, use the button below,
-          then drag a box around the sequences you are interested in."),
-        actionButton("button2.view", "Activate selection mode")
-      ),
+        # h4("Identify sequences"),
+        # p("To identify points in the sequence space, use the button below,
+        #   then drag a box around the sequences you are interested in.")     #,
+        # actionButton("button2.view", "Activate selection mode")           ########### selection mode cannot work online yet (RGLwidget)
+        ),
       
       # Image legend
       # mainPanel(
@@ -47,66 +49,68 @@ shinyUI(fluidPage(
       # ),
       # Display selected points alignment
       mainPanel(
+        rglwidgetOutput("mainplot.view", width="100%"),
         h4("Selected sequences"),
         htmlOutput("fasta2.view",style = "font-family:Courier New ; min-width:500px")
       )
       
     )),
-
-      
+    
+    
     # TAB 2 -----------------------------------
     tabPanel("Find",fluidPage(
-  
-    # Inputs section
-    sidebarLayout(
-  
-      # Select superfamily
-      sidebarPanel(
-        h3("Locate a sequence"),
-        p("Input a query sequence to locate in the defensin sequence space. Since the defensins consist
-          of two superfamilies, you may indicate whether it is a cis-defensin of trans-defensin (if known).
-          Accuracy is best when the query sequence includes only the defensin domain."),
-        textAreaInput("query_sequence",resize="vertical",rows=3,
-                  label       = "Find query sequence",
-                  value       = "RECKTESNTFPGICITKPPCRKACISEKFTDGHCSKILRRCLCTKPC",
-                  placeholder = "Amino acid sequence"),
-        selectInput("query_type",
-                    label   = "Within superfamily (if known)",
-                    choices  = c("unknown","cis-Defensin","trans-Defensin"),
-                    selected = "unknown"),
-        p("Clicking \"calculate\" will open the interactive diagram in a new window."),
-        actionButton("button", "Calculate"),
-        tags$hr(style="border-color: grey;"),
-        h4("Identify sequences"),
-        p("To identify points in the sequence space, use the button below,
-          then drag a box around the sequences you are interested in."),
-        actionButton("button2", "Activate selection mode")
-        ),
-  
       
-      # Outputs section 
-      mainPanel(
+      # Inputs section
+      sidebarLayout(
         
-        # Display summary text
-        h4(textOutput("title")),
-        textOutput("report"),
-        tableOutput("table"),
-        # plotOutput("histplot",height = "250px"),
+        # Select superfamily
+        sidebarPanel(
+          h3("Locate a sequence"),
+          p("Input a query sequence to locate in the defensin sequence space. Since the defensins consist
+            of two superfamilies, you may indicate whether it is a cis-defensin of trans-defensin (if known).
+            Accuracy is best when the query sequence includes only the defensin domain."),
+          textAreaInput("query_sequence",resize="vertical",rows=3,
+                        label       = "Find query sequence",
+                        value       = "RECKTESNTFPGICITKPPCRKACISEKFTDGHCSKILRRCLCTKPC",
+                        placeholder = "Amino acid sequence"),
+          selectInput("query_type",
+                      label   = "Within superfamily (if known)",
+                      choices  = c("unknown","cis-Defensin","trans-Defensin"),
+                      selected = "unknown"),
+          p("Clicking \"calculate\" will open the interactive diagram"),
+          actionButton("button", "Calculate"),
+          # tags$hr(style="border-color: grey;"),
+          # h4("Identify sequences"),
+          # p("To identify points in the sequence space, use the button below,
+          #   then drag a box around the sequences you are interested in.")      
+          # actionButton("button2", "Activate selection mode")                 ########### selection mode cannot work online yet (RGLwidget)
+          ),
         
-        # Display fasta alignment
-        tags$hr(style="border-color: grey;"),
-        h4("Nearest neighbours"),
-        p("These are the sequences that are closest to the query sequence within the sequence space."),
-        sliderInput("return_nearest","Number of nearest neighbours",value=10,min=1,max=100),
-        htmlOutput("fasta",style = "font-family:Courier New ; min-width:500px"),
         
-        # Display selected points alignment
-        tags$hr(style="border-color: grey;"),
-        h4("Selected sequences"),
-        htmlOutput("fasta2",style = "font-family:Courier New ; min-width:500px")
-      )
-    )
-  ))),
+        # Outputs section 
+        mainPanel(
+          
+          # Display summary text
+          rglwidgetOutput("mainplot.query",width="100%"),
+          h4(textOutput("title")),
+          textOutput("report"),
+          tableOutput("table"),
+          # plotOutput("histplot",height = "250px"),
+          
+          # Display fasta alignment
+          tags$hr(style="border-color: grey;"),
+          h4("Nearest neighbours"),
+          p("These are the sequences that are closest to the query sequence within the sequence space."),
+          sliderInput("return_nearest","Number of nearest neighbours",value=10,min=1,max=100),
+          htmlOutput("fasta",style = "font-family:Courier New ; min-width:500px"),
+          
+          # Display selected points alignment
+          tags$hr(style="border-color: grey;"),
+          h4("Selected sequences"),
+          htmlOutput("fasta2",style = "font-family:Courier New ; min-width:500px")
+        )
+        )
+      ))),
   
   # Footer --------------------------
   wellPanel(
@@ -122,11 +126,9 @@ shinyUI(fluidPage(
           a(href="https://doi.org/10.1007/s00018-016-2344-5","DOI:10.1007/s00018-016-2344-5"))
     )
   ),
-  p("AlignStat was developed by Thomas Shafee at the ",
+  p("DefSpace was developed by Thomas Shafee at the ",
     a(href="http://www.latrobe.edu.au/lims","La Trobe Institute of Molecular Science (LIMS)")," and ",
     a(href="http://hexima.com.au/","Hexima")),
   img(src="http://cysbar.science.latrobe.edu.au/img/lims.png",width="200px"),
-  img(src="http://cysbar.science.latrobe.edu.au/img/hexima.png",width="200px"),
-  plotOutput("mainplot"),
-  plotOutput("mainplot.view")
-))
+  img(src="http://cysbar.science.latrobe.edu.au/img/hexima.png",width="200px")
+        ))
